eddde3960929c69fe49f3cc19500910b38b1f650
asciphx <maxspore@foxmail.com>
2020-09-14 14:32:25 +0800

    fastest

--- ts/controller.ts
+++ ts/controller.ts
@@ -2,21 +2,21 @@ import { Context } from "koa";
 //基础控制器
 export abstract class Controller {
   [x: string]: any;
-  async add(ctx: Context) {
+  private async add(ctx: Context) {
     ctx.body = await this.save(ctx.request.body);
   }
-  async del(ctx: Context) {
+  private async del(ctx: Context) {
     ctx.body = await this.remove(ctx.params.id);
   }
-  async fix(ctx: Context) {
+  private async fix(ctx: Context) {
     ctx.body = await this.update(ctx.params.id, ctx.request.body);
   }
-  async info(ctx: Context) {
+  private async info(ctx: Context) {
     let v = await this.findOne(ctx.params.id);
     if (!v) { ctx.status = 404; return; }
     ctx.body = v;
   }
-  async page(ctx: Context) {
+  private async page(ctx: Context) {
     ctx.body = await this.list(ctx.query);
   }
 }
--- ts/service.ts
+++ ts/service.ts
@@ -5,34 +5,34 @@ interface _ {
 }
 //基础服务类
 export abstract class Service{
-  protected static service:string;
-  protected static _: _;
+  private service:string;
+  private _: _;
   constructor(_:_){
-    Service.service=this.constructor.name.replace(/(\w*)[A-Z]\w*/,"$1").toLowerCase();
-    Service._=_;
+    this.service=this.constructor.name.replace(/(\w*)[A-Z]\w*/,"$1").toLowerCase();
+    this._=_;
   }
-  async save(obj) {
-    return this[Service.service].save(obj);
+  private async save(obj) {
+    return this[this.service].save(obj);
   }
-  async update(id: number, obj) {
-    return this[Service.service].update(id, obj);
+  private async update(id: number, obj) {
+    return this[this.service].update(id, obj);
   }
-  async remove(id: number) {
-    let rm = await this[Service.service].findOne(id);
-    return this[Service.service].remove(rm);
+  private async remove(id: number) {
+    let rm = await this[this.service].findOne(id);
+    return this[this.service].remove(rm);
   }
-  async findOne(id: number) {
-    return this[Service.service].findOne(id);
+  private async findOne(id: number) {
+    return this[this.service].findOne(id);
   }
-  async list(query) {
+  private async list(query) {
     let { pageNum = 10, current = 1 } = query;//只需接收每页多少与当前页，默认每页10个，当前第一页
-    let v=(this[Service.service]as Repository<ObjectLiteral>).createQueryBuilder()
+    let v=(this[this.service]as Repository<ObjectLiteral>).createQueryBuilder()
       .take(pageNum).skip(current*pageNum-pageNum);
-    if(Service._){
-      if(Object.keys(Service._.orderBy).length!==0){
-        for (const key in Service._.orderBy) {v.addOrderBy(key,Service._.orderBy[key].toUpperCase()) }
+    if(this._){
+      if(Object.keys(this._.orderBy).length!==0){
+        for (const key in this._.orderBy) {v.addOrderBy(key,this._.orderBy[key].toUpperCase()) }
       }
-      if(Service._.where){v.where(Service._.where(query))}
+      if(this._.where){v.where(this._.where(query))}
     }
     return {list:await v.getMany(),
       page:new Page(current,pageNum,await v.getCount(),current*pageNum-pageNum).get()};
