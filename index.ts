73bfc752a805197f15503fa853bb6784af436559
asciphx <maxspore@foxmail.com>
2020-09-16 01:44:09 +0800

    encryption algorithm

--- index.ts
+++ index.ts
@@ -17,14 +17,14 @@ createConnection().then(async connection => {Tag.Init(connection.name);//Require
     const token = ctx.headers.authorization;
     if(token){
       try {
-        Jwt.verify(token.split(' ')[1],ctx.headers.secret,{complete:true});await next();
+        Jwt.verify(token.split(' ')[1],Number("0x"+ctx.headers.secret).toString(36),{complete:true});await next();
       } catch (e) {
         ctx.status=401;ctx.body=String(e);
       }
     }else{
       ctx.status=401;ctx.body="Authentication Error";console.log(ctx.url)
     }
-  })//动态secret,基于前后端共识算法，采用62进制转36进制(因账户名只能是数字或者英文字母共62个)。
+  })//动态secret,基于前后端共识算法，采用二次转，62进制转16进制+后端补码0x再转36进制(因账户名只能是数字或者英文字母共62个)。
   const router = new Router();//console.log(Routes)
   Routes.forEach(r => {
     router[r.m](...r.w?[r.r,...r.w]:[r.r],async(ctx:Koa.Context,next)=>{
