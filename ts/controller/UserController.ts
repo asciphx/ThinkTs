b96702bb134f1facbe7769869314cbb346097bdb
asciphx <maxspore@foxmail.com>
2020-09-15 13:56:07 +0800

    customize variable name

--- ts/controller/UserController.ts
+++ ts/controller/UserController.ts
@@ -6,16 +6,16 @@ import { Controller } from '../controller';
 //自动实现curd,第一参数可省
 @Class("user",["add", "del", "fix", "info", "page"])
 class UserController extends Controller {
-  @Service(UserService) readonly user_: UserService
+  @Service(UserService) readonly u_: UserService
   
   @Roles(W.Qx)
   @Post("register")
   async register(ctx: Context) {
-    ctx.body = await this.user_.register(ctx.request.body);
+    ctx.body = await this.u_.register(ctx.request.body);
   }
   @Roles(W.Qx)
   @Post("login")
   async login(ctx: Context) {
-    ctx.body = await this.user_.login(ctx.request.body);
+    ctx.body = await this.u_.login(ctx.request.body);
   }
 }
--- ts/service/AdminService.ts
+++ ts/service/AdminService.ts
@@ -4,12 +4,12 @@ import { Service, Inject, container } from "../service";
 
 export class AdminService extends Service {
   constructor(
-    @Inject(Admin) private ad=container.get(Admin.name)
+    @Inject(Admin) private adm=container.get(Admin.name)
   ) {
     super({
       where: query => {
         return new Brackets(qb => {
-          if (query.account) qb.where('account like :account', { account: `%${query.account}%` })
+          if (query.name) qb.where('name like :name', { name: `%${query.name}%` })
           if (query.id) qb.andWhere('id >:id', { id: query.id })
         });
       },
