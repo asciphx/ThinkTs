0077c29f30f2c9aa7fb029d8a2a8a8517bec6d84
asciphx <maxspore@foxmail.com>
2020-09-21 00:16:25 +0800

    fix:config

--- ormconfig.js
+++ ormconfig.js
@@ -6,8 +6,6 @@ module.exports = {
     "password": "fastjs",
     "database": "spring",
     "synchronize": true,
-    "logging": false,
     "entities": [`${process.env.NODE_ENV==='production'?'dist':'ts'}/entity/*{.ts,.js}`],
-    "synchronize": true,
     // "logging": [ "query", "error" ]
 };
