f6729e875f70a7e555c2d4c2c8380772e5dbc0ed
asciphx <maxspore@foxmail.com>
2020-09-21 21:57:16 +0800

    update

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,7 @@ module.exports = {
     "host": "127.0.0.1",
     "port": 3306,
     "username": "root",
-    "password": "fastjs",
+    "password": "",
     "database": "spring",
     "synchronize": true,
     "entities": [`${process.env.NODE_ENV==='production'?'dist':'ts'}/entity/*{.ts,.js}`],
--- ts/config.ts
+++ ts/config.ts
@@ -1,12 +1,12 @@
 const Conf={
   printRoute:true,//打印路由到routes目录
-  port:3000,
+  port:8080,
   DATABASE:"",//启动时匹配ormconfig.json中的数据库
   upload:"upload",
   expiresIn:"10h",//10个小时内jwt不会过期
   cipher:0x24,//最大值0x24，最小是0x2，静态的
   secret:19+Math.random()*17|0,//19~35
-  unless:/^\/static\/*|\/user\/register|\/user\/login|\/login.html|\/favicon.ico/,
+  unless:/^\/static\/*|\/user\/register|\/user\/login|\/login.html|\/|\/favicon.ico/,
   jsonLimit:"1mb",
   view:'../views'
 } 
