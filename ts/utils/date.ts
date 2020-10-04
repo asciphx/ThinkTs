f9cf7f71dfe5f50798276d715c1f9845706f0560
asciphx <maxspore@foxmail.com>
2020-10-04 13:46:28 +0800

    ad date

--- ts/utils/date.ts
+++ ts/utils/date.ts
@@ -2,16 +2,17 @@ const week = {"0" : "\u65e5","1" : "\u4e00","2" : "\u4e8c",
 "3" : "\u4e09","4" : "\u56db","5" : "\u4e94","6" : "\u516d"};
 export default {
     //time:new Date().getTime(),简易的获取时间
-    formateDate(time:number){
+    formateDate(time:number=Date.now()){
         if(!time) return '';
         let date = new Date(time)
-        return date.getFullYear() + '-' + (date.getMonth()+1).toString().padStart(2,'0')+'-'+date.getDate().toString().padStart(2,'0')+'  '+date.getHours().toString().padStart(2,'0')+':'+date.getMinutes().toString().padStart(2,'0')+':'+date.getSeconds().toString().padStart(2,'0') 
+        return date.getFullYear() + '-' + (date.getMonth()+1).toString().padStart(2,'0')+'-'+date.getDate().toString().padStart(2,'0')+' '+date.getHours().toString().padStart(2,'0')+':'+date.getMinutes().toString().padStart(2,'0')+':'+date.getSeconds().toString().padStart(2,'0') 
     },
-    /* new Date().pattern("yyyy-MM-dd hh:mm:ss.S") => 2019-08-02 08:09:04.423 
+    /* 
+    * new Date().pattern("yyyy-MM-dd hh:mm:ss.S") => 2019-08-02 08:09:04.423 
     * new Date().pattern("yyyy-MM-dd EE hh:mm:ss") => 2019-08-10 周六 08:09:04       
-    * new Date().pattern("yyyy-MM-dd EEE hh:mm:ss") => 2019-08-10 星期六 08:09:04       
-    * 其他格式 MM/dd/yyyy HH  MM/dd/yyyy */
-    date2str(date:Date,fmt:string){
+    * 其他格式 MM/dd/yyyy HH  MM/dd/yyyy
+    */
+    date2str(date:Date=new Date,fmt:string="yyyy-MM-dd EEE hh:mm:ss"){
       let o = {"M+" : date.getMonth()+1,"d+" : date.getDate(),
       "h+" : date.getHours()%12 === 0 ? 12 : date.getHours()%12,
       "H+" : date.getHours(),"m+" : date.getMinutes(),
--- ts/weblogic.ts
+++ ts/weblogic.ts
@@ -1,10 +1,11 @@
 import { Context } from "koa"
+import date from "./utils/date"
 
 export const W = {
   async Log(ctx: Context, next) {
     const start = Date.now();
     await next();
-    console.log(`${ctx.request.method}${ctx.request.url} used ${Date.now() - start}ms`); 
+    console.log(`${ctx.method}${ctx.url} used ${Date.now() - start}ms -> ${date.date2str()}`); 
   },
   async Any(ctx: Context, next) {
     //Anything else
