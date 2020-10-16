e4589b751cfba62d6f366423fd0914ea6d7db668
asciphx <maxspore@foxmail.com>
2020-10-16 18:11:51 +0800

    Optimization date.ts

--- ts/utils/date.ts
+++ ts/utils/date.ts
@@ -1,4 +1,4 @@
-const week = {"0":"\u65e5","1":"\u4e00","2":"\u4e8c","3":"\u4e09","4":"\u56db","5":"\u4e94","6":"\u516d"};
+const week = {0:"\u65e5",1:"\u4e00",2:"\u4e8c",3:"\u4e09",4:"\u56db",5:"\u4e94",6:"\u516d"};
 export default {
   /**简易的获取时间
   * @param time:Date.now()
@@ -9,25 +9,25 @@ export default {
     date = null;return r
   },
   /* 
-  * date2str(new Date,"YYYY-MM-DD hh:mm:ss.S") => 2019-08-02 08:09:04.423 
-  * date2str(new Date,"YYYY-MM-DD EE hh:mm:ss") => 2019-08-10 周六 08:09:04       
+  * date2str("YYYY-MM-DD hh:mm:ss.S",new Date) => 2019-08-02 下午 08:09:04.423 
+  * date2str("YYYY-MM-DD EE HH:mm:ss") => 2019-08-10 周六 20:09:04       
   * 其他格式 MM/DD/YYYY HH  MM/DD/YYYY
   */
-  date2str(date:Date=new Date,fmt:string="YYYY-MM-DD EEE hh:mm:ss"){
+  date2str(f:string="YYYY-MM-DD EEE hh:mm:ss",date:Date=new Date){
     let o = {"M+":date.getMonth()+1,"D+":date.getDate(),
-    "h+":date.getHours()%12 === 0 ? 12:date.getHours()%12,
+    "h+":date.getHours() === 12 ? 12:date.getHours()%12,
     "H+":date.getHours(),"m+":date.getMinutes(),
     "s+":date.getSeconds(),"q+":Math.floor((date.getMonth()+3)/3),
     "S":date.getMilliseconds()};
-    if(/(Y+)/.test(fmt)){fmt=fmt.replace(RegExp.$1,(date.getFullYear()+"").substr(4 - RegExp.$1.length));}
-    if(/(E+)/.test(fmt)){fmt=fmt.replace(RegExp.$1,(RegExp.$1.length>1 ?
-      (RegExp.$1.length>2 ? "\u661f\u671f":"\u5468"):"")
-      +unescape(week[date.getDay()+""]));}
-    for(let k in o){
-      if(new RegExp("("+ k +")").test(fmt)){
-        fmt = fmt.replace(RegExp.$1, (RegExp.$1.length===1) ?
+    if(/(Y+)/.test(f)){f=f.replace(RegExp.$1,(date.getFullYear()+"").substr(4 - RegExp.$1.length));}
+    if(/(E+)/.test(f)){f=f.replace(RegExp.$1,(RegExp.$1.length>2 ?"\u661f\u671f":RegExp.$1.length>1?"\u5468":"")
+      +week[date.getDay()]);}
+    if(/(h+)/.test(f)){f=f.replace(/(?=h+)/,date.getHours()>12?"\u4E0B\u5348 ":"\u4E0A\u5348 ")}
+    for(let k in o)
+      if(new RegExp("("+ k +")").test(f)){
+        f = f.replace(RegExp.$1, (RegExp.$1.length===1) ?
         o[k]:("00"+ o[k]).substr((""+ o[k]).length));
       }
-    }o=date=null;return fmt;
+    o=date=null;return f;
   }
 }
