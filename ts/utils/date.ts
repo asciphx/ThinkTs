b7bf69166cd1ea2cf195012be2e2504604f20c54
asciphx <maxspore@foxmail.com>
2020-10-16 17:33:53 +0800

    fix

--- ts/utils/date.ts
+++ ts/utils/date.ts
@@ -4,7 +4,7 @@ export default {
   * @param time:Date.now()
   */
   formateDate(time:number){
-    let date = new Date(time)
+    if(!time) return '';let date = new Date(time)
     let r = date.getFullYear()+'-'+(date.getMonth()+1).toString().padStart(2,'0')+'-'+date.getDate().toString().padStart(2,'0')+' '+date.getHours().toString().padStart(2,'0')+':'+date.getMinutes().toString().padStart(2,'0')+':'+date.getSeconds().toString().padStart(2,'0')
     date = null;return r
   },
