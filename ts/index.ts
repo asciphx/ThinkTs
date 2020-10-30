ae40da1145a1ce2dc34aa7113c8728b05cf054f1
asciphx <maxspore@foxmail.com>
2020-10-30 18:25:31 +0800

    update 1.9.8

--- ts/index.ts
+++ ts/index.ts
@@ -22,33 +22,34 @@ createConnection().then(async conn => {Tag.Init(conn.name);//Require to use deco
     if (ctx.method === 'OPTIONS') { ctx.body=200; }
     if(ctx.url.match(Conf.unless)||Conf.noJwt){await next();return}
     const token:string=ctx.headers.a,s:string=ctx.headers.s?ctx.headers.s.match(/[^#]+/g):null;
-    if(token&&s){
+    if(token&&s){let ll:Array<any>,l:Array<string>
       try {
         let {payload}=Jwt.verify(token.replace(/^Bearer /,""),
           NTo10(s[0],Number("0x"+s[1])/0x4F).toString(Conf.cipher),{complete:true}) as any;
-        let ll:Array<any>=Object.entries(payload)[0], l=ll[1] as Array<string>;//role list
-        const path=ctx.url.replace(/\d+|(\w+)\?.+$/,"$1")
-        for (let i = 0; i < l.length; i++) {
-          if(Maps.hasOwnProperty(l[i])){
-            if(Maps[l[i]].includes(ctx.method+path)){await next();return}
-            if(Date.now()-(fristTime[l[i]]||0)>Conf.synchronize){fristTime[l[i]]=Date.now()}else continue;const url=await Redis.get(l[i])
-            if(url.match(ctx.method+path)){Maps[l[i]]=url.split(",");await next();return}continue;
-          }
-          let m=await (Cache[Menu.name]as Repository<Menu>).createQueryBuilder("m").leftJoin("m.roles","role")
-          .select("m.path").where(`role.name ="${l[i]}"`).getMany();
-          if(m.length>0){m.forEach((e,i,l)=>{(l[i] as any)=e.path});Maps[l[i]]=m;Redis.set(l[i],m.toString());}
-          if((m as any).includes(ctx.method+path)){await next();m=null;return}m=null;
-        }
-        if(ll[0]==="admin"){await next();return}
-        ctx.status=403;ctx.body=`'${ctx.method+path}' request is not authorized`;l=ll=payload=null
+        ll=Object.entries(payload)[0],l=ll[1];payload=null;
       } catch (e) {
         if(String(e).includes('TokenExpiredError')){ ctx.status=401;ctx.body="Jwt Expired";
         }else if(String(e).includes('QueryFailedError')){ctx.status=406;ctx.body=e;
         }else{console.error(e);ctx.status=401;ctx.body="Authentication Error";}
       }
+      const path=ctx.url.replace(/\d+|(\w+)\?.+$/,"$1")
+      for (let i = 0; i < l.length; i++) {
+        if(Maps.hasOwnProperty(l[i])){
+          if(Date.now()-(fristTime[l[i]]||0)>Conf.synchronize){fristTime[l[i]]=Date.now()}else{
+            if(Maps[l[i]].includes(ctx.method+path)){await next();return}continue
+          };const url=await Redis.get(l[i])
+          if(url.match(ctx.method+path)){Maps[l[i]]=url.split(",");await next();return}continue;
+        }
+        let m=await (Cache[Menu.name]as Repository<Menu>).createQueryBuilder("m").leftJoin("m.roles","role")
+        .select("m.path").where(`role.name ="${l[i]}"`).getMany();
+        if(m.length>0){m.forEach((e,i,l)=>{(l[i] as any)=e.path});Maps[l[i]]=m;Redis.set(l[i],m.toString());}
+        if((m as any).includes(ctx.method+path)){await next();m=null;return}m=null;
+      }
+      if(ll[0]==="admin"){await next();return}
+      ctx.status=403;ctx.body=`'${ctx.method+path}' request is not authorized`;l=ll=null
     }else{ctx.status=401;ctx.body="Headers Error";}
   });
-  setInterval(()=>{Conf.secret=11+Math.random()*25|0;},1414);//每1.414秒换一次私钥
+  setInterval(()=>{Conf.secret=11+Math.random()*25|0;},1414);
   Conf.DATABASE = conn.driver.database;const router = new Router();//console.log(Routes)
   Routes.forEach(r => {
     router[r.m](...r.w?[r.r,r.w]:[r.r],async(ctx:Koa.Context,next)=>{
@@ -63,4 +64,4 @@ createConnection().then(async conn => {Tag.Init(conn.name);//Require to use deco
     new User({account:"admin",pwd:encrypt("654321","shake256","base64",28)} as User))
     .then(user => {console.log("User has been saved: ", user);
   })
-})//.catch(e => {console.error(e)});
+})
