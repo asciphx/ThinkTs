242e65de0232ef3d0d9e39ac9f62bff96f0660c1
asciphx <maxspore@foxmail.com>
2020-10-31 14:03:40 +0800

    update 2.0.0

--- ts/index.ts
+++ ts/index.ts
@@ -11,7 +11,7 @@ createConnection().then(async conn => {Tag.Init(conn.name);//Require to use deco
     let en=require(__dirname+"/entity/"+i),key=Object.keys(en)[0];Cache[key]=getRepository(en[key]);en=null
   });let fristTime={};
   await fs.readdirSync(__dirname+"/controller").forEach((i)=>{require(__dirname+"/controller/"+i)})
-  const app = new Koa().use(bodyParser({ jsonLimit: Conf.jsonLimit, formLimit: "3mb", textLimit: "2mb" }))
+  const APP = new Koa().use(bodyParser({ jsonLimit: Conf.jsonLimit, formLimit: "3mb", textLimit: "2mb" }))
   .use(views(path.join(__dirname,Conf.view),{autoRender:false,extension: 'html',map: { html: "ejs" }}))
   .use(koaStatic(path.join(__dirname,Conf.view),{defer:true}))
   .use(async (ctx, next) => {
@@ -21,27 +21,29 @@ createConnection().then(async conn => {Tag.Init(conn.name);//Require to use deco
     ctx.set('Access-Control-Allow-Credentials',"true");
     if (ctx.method === 'OPTIONS') { ctx.body=200; }
     if(ctx.url.match(Conf.unless)||Conf.noJwt){await next();return}
-    const token:string=ctx.headers.a,s:string=ctx.headers.s?ctx.headers.s.match(/[^#]+/g):null;
-    if(token&&s){
+    const TOKEN:string=ctx.headers.a,S:string=ctx.headers.s?ctx.headers.s.match(/[^#]+/g):null;
+    if(TOKEN&&S){
       try {
-        let {payload}=Jwt.verify(token.replace(/^Bearer /,""),
-          NTo10(s[0],Number("0x"+s[1])/0x4F).toString(Conf.cipher),{complete:true}) as any;
+        let {payload}=Jwt.verify(TOKEN.replace(/^Bearer /,""),
+          NTo10(S[0],Number("0x"+S[1])/0x4F).toString(Conf.cipher),{complete:true}) as any;
         let ll:Array<any>=Object.entries(payload)[0],l:Array<string>=ll[1];
-        const path=ctx.url.replace(/\d+|(\w+)\?.+$/,"$1")
-        for (let i = 0; i < l.length; i++) {
-          if(Maps.hasOwnProperty(l[i])){
-            if(Date.now()-(fristTime[l[i]]||0)>Conf.synchronize){fristTime[l[i]]=Date.now()}else{
-              if(Maps[l[i]].includes(ctx.method+path)){await next();return}continue
-            };const url=await Redis.get(l[i])
-            if(url.match(ctx.method+path)){Maps[l[i]]=url.split(",");await next();return}continue;
+        const PATH=ctx.method+ctx.url.replace(/\d+|(\w+)\?.+$/,"$1")
+        for (let i = 0; i < l.length; i++) {const ROLE:string=l[i];
+          if(Maps.hasOwnProperty(ROLE)){
+            if(Date.now()-(fristTime[ROLE]||0)>Conf.synchronize){fristTime[ROLE]=Date.now()}else{
+              if(Maps[ROLE].includes(PATH)){await next();return}continue
+            };const URL=await Redis.get(ROLE)
+            if(URL.match(PATH)){Maps[ROLE]=URL.split(",");await next();return}else{
+              const IDEX=Maps[ROLE].findIndex(v=>v===PATH);IDEX>-1&&Maps[ROLE].splice(IDEX,1);continue;
+            }
           }
           let m=await (Cache[Menu.name]as Repository<Menu>).createQueryBuilder("m").leftJoin("m.roles","role")
-          .select("m.path").where(`role.name ="${l[i]}"`).getMany();
-          if(m.length>0){m.forEach((e,i,l)=>{(l[i] as any)=e.path});Maps[l[i]]=m;Redis.set(l[i],m.toString());}
-          if((m as any).includes(ctx.method+path)){await next();m=null;return}m=null;
+          .select("m.path").where(`role.name ="${ROLE}"`).getMany();
+          if(m.length>0){m.forEach((e,i,l)=>{(l[i] as any)=e.path});Maps[ROLE]=m;Redis.set(ROLE,m.toString());}
+          if((m as any).includes(PATH)){await next();m=null;return}m=null;
         }
         if(ll[0]==="admin"){await next();return}l=ll=payload=null;
-        ctx.status=403;ctx.body=`'${ctx.method+path}' request is not authorized`;
+        ctx.status=403;ctx.body=`'${PATH}' request is not authorized`;
       } catch (e) {
         if(String(e).includes('TokenExpiredError')){ ctx.status=401;ctx.body="Jwt Expired";
         }else if(String(e).includes('QueryFailedError')){ctx.status=406;ctx.body=e;
@@ -56,10 +58,10 @@ createConnection().then(async conn => {Tag.Init(conn.name);//Require to use deco
       ctx.body=await r.a(ctx,next);
     })
   })
-  app.use(router.routes()).use(router.allowedMethods()).listen(Conf.port,"0.0.0.0",()=>
+  APP.use(router.routes()).use(router.allowedMethods()).listen(Conf.port,"0.0.0.0",()=>
     console.log(`ThinkTs run on http://localhost:${Conf.port}/test.html`))
-  const exist = await Cache[User.name].findOne({account:"admin"});
-  if (exist) {console.error("董事长已存在!");return;} else
+  const EXIST = await Cache[User.name].findOne({account:"admin"});
+  if (EXIST) {console.error("董事长已存在!");return;} else
   return Cache[User.name].save(
     new User({account:"admin",pwd:encrypt("654321","shake256","base64",28)} as User))
     .then(user => {console.log("User has been saved: ", user);
