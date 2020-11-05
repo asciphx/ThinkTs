fb7184edb5a7888ffb32d57794a3c4b020aeed0a
asciphx <maxspore@foxmail.com>
2020-11-05 18:35:17 +0800

    Change format

--- ts/view.ts
+++ ts/view.ts
@@ -1,22 +1,14 @@
-import { html, getFilesPath } from "./utils/tool";import * as fs from "fs"
-import { Class, Get, Q } from "./think/decorator";import { Maps } from "./config"
-import { Tag } from "./utils/tag";import { Context } from "koa"
+import { html, getFilesPath } from "./utils/tool";import { Tag } from "./utils/tag";import * as fs from "fs"
+import { Class, Get, Q } from "./think/decorator"; import { Maps } from "./config";import { Context } from "koa"
+
 @Class()
 class View {
-  @Get()
-  @Get("index.html")
-  @Get("login.html")
-  html(ctx: Context) {
-    return html(ctx, { test: "For ", author: "anyone" });
-  }
+  @Get() @Get("index.html") @Get("login.html")
+  html(ctx: Context) { return html(ctx, { test: "For ", author: "anyone" }); }
   @Get("_")
-  _() {
-    return Maps;
-  }
+  _() { return Maps; }
   @Get("static/tag")//前端渲染Tag,前缀static可以不被拦截
-  $(@Q q) {
-    return Tag.h(q.v,q.a,q.n,Number(q.t)as 0,q.c);
-  }
+  $(@Q q){ return Tag.h(q.v, q.a, q.n, Number(q.t) as 0, q.c) };
   @Get("static/tsx")//获取ts目录这一层的目录或者是文件的名称
   tsx() {
     let str=""
@@ -24,7 +16,5 @@ class View {
     return str.replace(/,$/,"")
   }
   @Get("static/ts")//获取ts路径下，所有文件的相对路径
-  ts() {
-    return getFilesPath("./ts")
-  }
+  ts() { return getFilesPath("./ts") }
 }
