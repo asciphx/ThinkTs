a4aaaece6891db32cff052b006dcc43da6eafa01
asciphx <maxspore@foxmail.com>
2020-12-01 16:39:24 +0800

    fix

--- ts/controller/AdminController.ts
+++ ts/controller/AdminController.ts
@@ -5,17 +5,16 @@ import { Context } from "koa";import { html } from "../utils/tool";
 import { Tag } from "../utils/tag";import { W } from "../weblogic";
 
 @Class(["add", "del", "info", "fix", "page"])
-@Id("(\\d+)")//路由主键拦截，中间填的默认参数，此时可去掉该装饰器，不会影响
 class AdminController extends Controller {
   @Inject(AdminService) readonly adm_: AdminService
   
-  @app.get("sql")//可直接引入app，为像python那样，并且最上面import也能方便少写
+  @app.get("sql")
   sql(@Q q){
     return this.adm_.sql(q);
   }
 
   @Middle(W.Log)
-  @app.get("index.html")//允许后端模板渲染Tag。
+  @app.get("index.html")
   async index(ctx:Context){
     return html(ctx,{path:"admin",STATUS:await Tag.h("0","selected","STATUS",0,"input-text"),
         ID_TYPE:await Tag.h("0","checked","ID_TYPE",1,"input-radius"),
--- ts/controller/ParseController.ts
+++ ts/controller/ParseController.ts
@@ -3,7 +3,7 @@ import { Controller } from '../think/controller';
 import { ParseService } from "../service/ParseService";
 
 @Class(["add", "del", "fix", "info", "page"])
-@Id("([a-z]{1,9})")//表示只允许字母(1-9位无视大小写)，[降低无效请求带来的性能开销]
+@Id("([a-z]{1,9})")
 class ParseController extends Controller {
   @Inject(ParseService) readonly parse_: ParseService
 }
