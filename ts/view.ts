ba5d16a8dea751ba31af2a2fadf9ec5761f5c9d2
asciphx <maxspore@foxmail.com>
2020-12-01 16:42:50 +0800

    fix

--- ts/view.ts
+++ ts/view.ts
@@ -7,14 +7,14 @@ class View {
   html(ctx: Context) { return html(ctx, { test: "For ", author: "anyone" }); }
   @Get("_")
   _() { return Maps; }
-  @Get("static/tag")//前端渲染Tag,前缀static可以不被拦截
+  @Get("static/tag")
   $(@Q q){ return Tag.h(q.v, q.a, q.n, Number(q.t) as 0, q.c) };
-  @Get("static/tsx")//获取ts目录这一层的目录或者是文件的名称
+  @Get("static/tsx")
   tsx() {
     let str=""
     fs.readdirSync("./ts").forEach(i=>{str += i + ",";});
     return str.replace(/,$/,"")
   }
-  @Get("static/ts")//获取ts路径下，所有文件的相对路径
+  @Get("static/ts")
   ts() { return getFilesPath("./ts") }
 }
