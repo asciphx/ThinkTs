e8fa7bd2fc52ae19b7cd5cd7b0cef71b3ded5011
asciphx <maxspore@foxmail.com>
2021-02-04 06:22:46 +0800

    shell

--- UpNpm.sh
+++ UpNpm.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
 set -e
 for i in $(npm outdated --parseable --depth=0 | cut -d: -f5)
 do
-    npm install "$i"
+  if [[ $i == @* ]];then
+      npm i -S -D $i
+  else
+      npm i $i
+  fi
 done
-
 read -p "press enter end"
