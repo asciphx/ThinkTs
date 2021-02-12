b16d181b6742fe1a50336bfce4da730016026954
asciphx <maxspore@foxmail.com>
2021-02-13 04:35:02 +0800

    fastest

--- example/Normal/ts/conn.ts
+++ example/Normal/ts/conn.ts
@@ -3,15 +3,13 @@ import{encrypt}from"./utils/crypto";import{cleanAll}from"./think/decorator";impo
 import{User}from'./entity/User';import Tag from"./utils/tag";import{Conf,Cache,vType}from'./config';
 
 createConnection().then(async conn=>{Tag.Init(conn.name,9000);
-  Promise.all((await promise(fs.readdir)(__dirname+"/entity")).map(i => {
-    let en=require(__dirname+"/entity/"+i),key=Object.keys(en)[0];Cache[key]=getRepository(en[key]);en=null;
+  Promise.all((await promise(fs.readdir)(__dirname+"/entity")).map(i => {let en=require(__dirname+"/entity/"+i),
+    key=Object.keys(en)[0];Cache[key]=getRepository(en[key]);en=null;require(__dirname+"/controller/"+key+"Controller.ts")
     let res=(Cache[key] as Repository<ObjectLiteral>).metadata.ownColumns;key=key.toLocaleLowerCase();vType[key]={};
     res.forEach(r=>{let t=r.type;Object.defineProperty(vType[key],r.propertyName,{enumerable:true,writable:true,//@ts-ignore
     value:t==="datetime"?26:t.name==="Number"?9:t.name==="Boolean"?5:t==="tinyint"?2:t==="smallint"?4:
     t==="mediumint"?6:t==="bigint"?18:r.length===""?255:Number(r.length)});});
-  }));
-  Promise.all((await promise(fs.readdir)(__dirname+"/controller")).map(i => {require(__dirname+"/controller/"+i);}));
-  Conf.DATABASE=conn.driver.database;Conf.TYPE=conn.driver.options.type;
+  }));Conf.DATABASE=conn.driver.database;Conf.TYPE=conn.driver.options.type;
   console.log('\x1B[35;47m%s\x1B[49m',"loading router……");cleanAll();
 
   const EXIST=await Cache["User"].findOne({account:"admin"});
--- ts/conn.ts
+++ ts/conn.ts
@@ -3,17 +3,15 @@ import{encrypt}from"./utils/crypto";import{cleanAll}from"./think/decorator";impo
 import{User}from'./entity/User';import Tag from"./utils/tag";import{Conf,Cache,vType}from'./config';
 
 createConnection().then(async conn=>{Tag.Init(conn.name,9000);
-  Promise.all((await promise(fs.readdir)(__dirname+"/entity")).map(i => {
-    let en=require(__dirname+"/entity/"+i),key=Object.keys(en)[0];Cache[key]=getRepository(en[key]);en=null;
+  Promise.all((await promise(fs.readdir)(__dirname+"/entity")).map(i => {let en=require(__dirname+"/entity/"+i),
+    key=Object.keys(en)[0];Cache[key]=getRepository(en[key]);en=null;require(__dirname+"/controller/"+key+"Controller.ts")
     let res=(Cache[key] as Repository<ObjectLiteral>).metadata.ownColumns;key=key.toLocaleLowerCase();vType[key]={};
     res.forEach(r=>{let t=r.type;Object.defineProperty(vType[key],r.propertyName,{enumerable:true,writable:true,//@ts-ignore
     value:t==="datetime"?26:t.name==="Number"?9:t.name==="Boolean"?5:t==="tinyint"?2:t==="smallint"?4:
     t==="mediumint"?6:t==="bigint"?18:r.length===""?255:Number(r.length)});});
   }));
-  Promise.all((await promise(fs.readdir)(__dirname+"/controller")).map(i => {require(__dirname+"/controller/"+i);}));
   Conf.DATABASE=conn.driver.database;Conf.TYPE=conn.driver.options.type;
   console.log('\x1B[35;47m%s\x1B[49m',"loading router……");cleanAll();
-
   const EXIST=await Cache["User"].findOne({account:"admin"});
   if(EXIST){console.error('\x1B[33;1m%s\x1B[22m',"董事长已存在!\x1B[37m");return;}else
   return Cache["User"].save(new User({account:"admin",pwd:encrypt("654321","shake256","latin1",50)}as User))
