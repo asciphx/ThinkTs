8dc80a807fe3fd8be28e02cee442c75b89a99d3b
asciphx <maxspore@foxmail.com>
2021-02-17 02:08:13 +0800

    fix:Pagination

--- ts/utils/page.ts
+++ ts/utils/page.ts
@@ -2,16 +2,13 @@ interface Pagination {
   page?: number;// 当前页[参数]
   size?: number;// 每页多少条[参数]
   count?: number;// 条数
-  total?: number;// 总页数
-}
+}//总页数可由客户端计算出，为省资源，因此不再提供，例如Math.ceil(count/size)
 export class Page {
-	private page:number=1;private total:number;private count:number;private size:number=10;
-	public get():Pagination{return {page:this.page,count:this.count,total:this.total,size:this.size}}
+	private page:number=1;private count:number;private size:number=10;
+	public get():Pagination{return {page:this.page,count:this.count,size:this.size}}
 	constructor(page:number,size:number,count:number){
     this.page=page<1?1:page;
 		this.size=size<1?1:size;
 		this.count=count;
-    this.total=Math.ceil(count/size);
-		this.page=this.page>this.total?this.total:this.page;
 	}
 }
